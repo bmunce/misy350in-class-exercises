109c5d1870104b17acfd33340261f99a10160a36
Brandon Munce <brandonmunce@gmail.com>
2017-10-26 16:31:55 -0400

    js exercise added

--- 10-26-2017/main.js
+++ 10-26-2017/main.js
@@ -13,18 +13,16 @@ for (let i = 0; i < bucket_list.length; i++) {
 }
 
 // nested loop
-
 let movie_list1 = ["1movie", "2movie", "3movie"];
 let movie_list2 = ["12movie", "24movie", "2movie", "6movie", "3movie"];
 
 for (let i = 0; i < movie_list1.length; i++) {
-  for (let j = 0; j < movie_list2.length; i++) {
+  for (let j = 0; j < movie_list2.length; j++) {
     if (movie_list1[i] == movie_list2[j]) {
       console.log(movie_list1[i]);
     }
   }
 }
-
 // iterator
 
 movie_list1.forEach(movie => console.log(">>" + movie));
