5983be516a19c8968f09a5bfbc7b56cb2c2322d8
Brandon Munce <brandonmunce@gmail.com>
2017-10-31 13:44:52 -0400

    jQuery added

---
+++ 10-31-2017/jQuery-basics.js
@@ -0,0 +1,38 @@
+
+
+function main () {
+  // all code should be here
+  // document.getElementById('google').innerHTML = "Google";
+  $('#google').html('Google');
+  // let links = document.getElementsByClassName('my-link');
+  let links = $('.my-link');
+  links[0].innerHTML = "Twitter";
+  links[0].href = "http://twitter.com";
+  links[1].innerHTML = "Linked-in";
+  links[1].href = "http://linkedin.com";
+  links[1].style.color = "red";
+
+  // button click event
+  $('#heading-button').click(function() {
+    $('h1').css('color', 'red');
+    $('.my-link').hide();
+  });
+
+  $('#toggle-button').click(function() {
+    $('.my-link').toggle();
+  });
+
+  // hide and show main text
+  $('#main-text').hide();
+  $('#main-text').fadeIn(4000);
+
+  // hide projects
+  $('.projects').hide();
+
+  // show projects by clicking
+  $('.project-button').click(function() {
+      $(this).next().slideToggle(500);
+  });
+}
+
+$(document).ready(main);
